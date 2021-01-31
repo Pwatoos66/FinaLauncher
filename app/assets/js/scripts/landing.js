a90f3940bf844a77b411ac2c78540deb6222484f
Avanton66 <36775649+Avanton66@users.noreply.github.com>
2021-01-31 18:07:36 +0100

    Update landing.js

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -686,9 +686,9 @@ function dlAsync(login = true){
                 const gameStateChange = function(data){
                     data = data.trim()
                     if(SERVER_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Exploring the Realm!')
+                        DiscordWrapper.updateDetails('Go to Finalium: geopolitical and Europacraft')
                     } else if(GAME_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Sailing to Westeros!')
+                        DiscordWrapper.updateDetails('Go to Finalium: Geopolitical and Europacraft !')
                     }
                 }
 
